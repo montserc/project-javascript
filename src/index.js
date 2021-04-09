942d7cd46545bef2dae2e0d85b960b00e48405fd
montserc <mrcatasus@gmail.com>
2021-04-09 13:39:38 +0200

    Funcionalidades de mezclar y pedir cartas

--- src/index.js
+++ src/index.js
@@ -1,71 +1,119 @@
 //Modelo
 const suits7yM = ['Oros', 'Copas', 'Espadas', 'Bastos'];
+const suits7yMColors = ['goldenrod', 'red', 'blue', 'green'];
 const numbers7yM =[1, 2, 3, 4, 5, 6, 7, 10, 11, 12];
 const values7yM =[1, 2, 3, 4, 5, 6, 7, 0.5, 0.5, 0.5];
 
 class Card {
-  constructor(number, suit, value) {
-      this.number = number;
-      this.suit = suit;
-      this.value = value;
-  }
+  constructor(number, suit, color, value) {
+    this.number = number;
+    this.suit = suit;
+    this.color = color;
+    this.value = value;
+  }  
   toString() {
     return `Soy la carta ${this.number} de ${this.suit}`
   }
 }
 
 class Player {
-  constructor(name) {
-      this.name = name;
-      this.score = 0;
-      this.cards = [];
+  constructor(name, score, cards) {
+    this.name = name;
+    this.score = score;
+    this.cards = cards;
   }
   toString() {
     return `Soy el jugador ${this.name}`
   }
 }
 
+/* Aleatoriza un array según el algoritmo de Fisher-Yates */
+Array.prototype.shuffle = function() {
+  for (let i = this.length - 1 ; i > 0 ; i--) {
+      let j = Math.floor(i * Math.random());
+      let tmp = this[j];
+      this[j] = this[i];
+      this[i] = tmp;
+  }
+  return this;
+}
+
 let deck;
 document.addEventListener('DOMContentLoaded',function() {
   deck = new Array(); 
-  suits7yM.map((suit) => {
+  suits7yM.map((suit, suitIndex) => {
     for (let i = 0; i < numbers7yM.length; i++) {
-      deck.push(new Card(numbers7yM[i], suit, values7yM[i]));
+      deck.push(new Card(numbers7yM[i], suit, suits7yMColors[suitIndex], values7yM[i]));
     }
   });
-  renderInit();
+  deck.shuffle();
+  renderDeck();
 });
 
-let players;
-function renderInit() {
+function renderDeck() {
   let divDeck = document.getElementById('deck');  
+  divDeck.textContent = ''; 
   deck.forEach((card) => {
     let divCard = document.createElement('div');
     divCard.classList.add('card');
     divCard.textContent = card.toString();
+    divCard.style.borderColor = card.color;
     divDeck.append(divCard);
   });    
-  document.forms[0].addEventListener('submit', function (event) {
-    event.preventDefault();
-    const numOfPlayers = this.querySelector('input').value;
+}
+
+let players;
+document.getElementById('init').addEventListener('click', function (event) {
+  const input = document.getElementById('num-of-players');
+  const numOfPlayers = input.value;
+  // comprobar si la partida ja està inciada
+  if (numOfPlayers.trim()) {
     players = new Array(); 
-    for (let i = 0; i < numOfPlayers; i++ ) {
-      players.push(new Player(`${i+1}`));
+    for (let i = 0; i < numOfPlayers; i++) {
+      players.push(new Player(`${i+1}`,0,new Array()));
     }
-    let divPlayers = document.getElementById('players');  
-    players.forEach((player) => {
-      let buttonAskCard = document.createElement('button');
-      buttonAskCard.innerHTML = 'Pedir Carta';
-      buttonAskCard.addEventListener('click',askCard);
-      let divPlayer = document.createElement('div');
-      divPlayer.classList.add('player');
-      divPlayer.textContent = player.toString();
-      divPlayer.append(buttonAskCard);
-      divPlayers.append(divPlayer);
-    });   
+    console.table(players);
+    input.value = '';
+    renderInitPlayers();
+  }
+});
+
+function renderInitPlayers() {  
+  let divPlayers = document.getElementById('players');  
+  players.forEach((player) => {
+    let divPlayer = document.createElement('div');
+    divPlayer.classList.add('player');
+    divPlayer.setAttribute('id', player.name);
+    divPlayer.textContent = player.toString();      
+    divPlayers.append(divPlayer);
+    let buttonAskCard = document.createElement('button');
+    buttonAskCard.innerHTML = 'Pedir Carta';
+    buttonAskCard.addEventListener('click', function (event) {
+        event.preventDefault;
+        let card = deck.pop();
+        player.cards.push(card);
+        renderDeck();
+        renderPlayer(player);
+    });
+    divPlayer.append(buttonAskCard);
+    let buttonStop = document.createElement('button');
+    buttonStop.innerHTML = 'Me planto';
+    buttonStop.addEventListener('click', stopPlaying(player));
+    divPlayer.append(buttonStop);
+    divPlayer.append(document.createElement('div'));
+  });   
+}
+
+function renderPlayer (player) {
+  let divPlayerCards = document.getElementById(player.name).lastChild;
+  divPlayerCards.textContent = ''; 
+  player.cards.forEach((card) => {
+    let divCard = document.createElement('div');
+    divCard.classList.add('card');
+    divCard.textContent = card.toString();
+    divPlayerCards.append(divCard);
   });
 }
 
-function askCard (event) {
-  //Math.random() * deck.length();
+function stopPlaying (event, player) {
 }
